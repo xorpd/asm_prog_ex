0b4fd223310ea712b88df36ca23f1bfbda50476b
Daan Kuijsten <daan@kuijstenwebcompany.nl>
2016-06-08 09:51:08 +0200

    added answers for 3_basic_fasm/

---
+++ 3_basic_fasm/4_answers/3_fix_programs/0_broken_adder.asm
@@ -0,0 +1,46 @@
+; Basic fasm Answers
+; ==================
+;
+; Notes:
+; The program at the bottom of the page is the fixed one
+; 
+; Fix Programs
+; ------------
+;
+; Broken Adder
+; @@@@@@@@@@@@
+; 
+; 0.    Try to assemble and find out the assembly error message.
+;
+; 1.    Fix the code to make the assembly process succeed.
+;
+; 2.    Run the program. Make sure that the program behaves correctly.
+;
+
+format PE console
+entry start
+
+include 'win32a.inc' 
+
+; ===============================================
+section '.text' code readable executable
+
+start:
+    ; The program begins here:
+
+    ; Read two numbers:
+    call    read_hex    
+    mov     esi,eax
+    call    read_hex
+
+    ; Add the two numbers, to get their sum.
+    add     eax,esi
+
+    ; Output the sum:
+    call    print_eax   
+
+    ; Exit the process:
+	push	0
+	call	[ExitProcess]
+
+include 'training.inc'
