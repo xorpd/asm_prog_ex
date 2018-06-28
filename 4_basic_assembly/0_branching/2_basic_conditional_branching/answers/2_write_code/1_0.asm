156c11a0b2edc43a7bdd50a0f15d315ea0dc3a34
xorpd <xorpd@xorpd.net>
2018-06-28 22:09:29 +0300

    Changed js -> jc, added jz, Fixed tabs.

--- 4_basic_assembly/0_branching/2_basic_conditional_branching/answers/2_write_code/1_0.asm
+++ 4_basic_assembly/0_branching/2_basic_conditional_branching/answers/2_write_code/1_0.asm
@@ -4,11 +4,11 @@
 ;     NOTE: The comparison should be in the unsigned sense. That means for
 ;     example: 0x00000003 < 0x7f123456 < 0xffffffff
 ;
-;		ANSWER:
+;       ANSWER:
 ;
-;			With subtraction we can compare if 1 number is larger then the other.
-;			Subtract z from y and jump when the number is signed. Then subtract
-;			y from x and jump when the number is signed.
+;           With subtraction we can compare if 1 number is larger then the other.
+;           Subtract z from y and jump when the number is signed. Then subtract
+;           y from x and jump when the number is signed.
 
 format PE console
 entry start
@@ -19,39 +19,41 @@ include 'win32a.inc'
 section '.text' code readable executable
 
 start:
-	call 	read_hex
-	; store x in ebx
-	mov		ebx, eax
+    call    read_hex
+    ; store x in ebx
+    mov     ebx, eax
 
-	call 	read_hex
-	; store y in ecx
-	mov		ecx, eax
+    call    read_hex
+    ; store y in ecx
+    mov     ecx, eax
 
-	call 	read_hex
-	; store z in edx
-	mov		edx, eax
+    call    read_hex
+    ; store z in edx
+    mov     edx, eax
 
-	sub 	edx, ecx
-	js		print_0
+    sub     edx, ecx
+    jc      print_0
+    jz      print_0
 
-	sub 	ecx, ebx
-	js		print_0
+    sub     ecx, ebx
+    jc      print_0
+    jz      print_0
 
-	call	print_1
-	
+    call    print_1
+    
 print_0:
-	mov		eax, 0
-	call	print
+    mov     eax, 0
+    call    print
 
 print_1:
-	mov		eax, 1
-	call	print
+    mov     eax, 1
+    call    print
 
 print:
-	call	print_eax
+    call    print_eax
 
     ; Exit the process:
-	push	0
-	call	[ExitProcess]
+    push    0
+    call    [ExitProcess]
 
 include 'training.inc'
